088e0ddc46ea426fdde67bb4c1d75b0bbb374097
Parsa Hosseini <parsah@Parsas-MacBook-Air.local>
2021-08-13 00:04:10 +0430

    Fix cors issue.

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = express()
 app.use(express.json());
